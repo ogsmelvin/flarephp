1f9e003c83245165763a11271816c87150f09978
Anthony De Leon <tonton.dev001@gmail.com>
2013-04-05 02:58:55 -0600

    rollback condition

--- core/ADK/Http/Uri.php
+++ core/ADK/Http/Uri.php
@@ -105,7 +105,7 @@ class Uri
         }
 
         $this->_host = isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : $_SERVER['HTTP_HOST'];
-        if($this->_port == '80'){
+        if($this->_port == '80' || $this->_protocol == 'https://'){
             $this->_baseUrl = $this->_protocol.$this->_host.$this->_baseUrl;
         } else {
             $this->_baseUrl = $this->_protocol.$this->_host.':'.$this->_port.$this->_baseUrl;
