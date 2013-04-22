c0359c943d561fa52b4e79b62d1c85ea620f790c
Anthony De Leon <tonton.dev001@gmail.com>
2013-04-22 23:45:52 +0800

    Commit change

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 $appDirectory = dirname(dirname(__FILE__));
 
-require $appDirectory.'/system/adk.php';
-use ADK\Adk as A;
+require $appDirectory.'/system/fphp.php';
+use FPHP\Fphp as A;
 
 A::mvc()->setAppDirectory($appDirectory.'/application')->start();
