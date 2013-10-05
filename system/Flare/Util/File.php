38e8cfa8db5f0d59940745e035f4626c5bdd64d6
Anthony De Leon <tonton.dev001@gmail.com>
2013-10-05 13:05:52 +0800

    Commit

---
+++ system/Flare/Util/File.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Flare\Util;
+
+use \RuntimeException;
+use \SplFileObject;
+
+/**
+ * 
+ * @author anthony
+ * 
+ */
+class File extends SplFileObject
+{
+    /**
+     * 
+     * @var boolean
+     */
+    private $_isValid = true;
+
+    /**
+     * 
+     * @param string $path
+     * @param string $openmode
+     * @param boolean $use_include_path
+     */
+    public function __construct($path, $openmode = 'r', $use_include_path = false)
+    {
+        try {
+            parent::__construct($path, $openmode, $use_include_path);
+        } catch (RuntimeException $ex) {
+            $this->_isValid = false;
+        }
+    }
+
+    /**
+     * 
+     * @return boolean
+     */
+    public function exists()
+    {
+        return $this->_isValid;
+    }
+}
